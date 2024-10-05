35ae8b04363e06403ef2b64c63d61d77e4ee233b
Alfredo Medrano <alfredomedranosanchez@gmail.com>
2024-10-05 11:38:43 -0600

    add basic crud

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,75 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type User struct {
+	Id    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
 
 func main() {
-	fmt.Println("Works")
+	router := gin.Default()
+	users := []User{}
+	indexUser := 1
+	fmt.Println("Running app")
+	// Tomar archivos de la carpeta template
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index.html", gin.H{})
+	})
+	// API URLs
+	//Obtener usuarios
+	router.GET("/api/users", func(c *gin.Context) {
+		c.JSON(200, users)
+	})
+	//Creación de usuarios
+	router.POST("/api/users", func(c *gin.Context) {
+		var user User
+		if c.BindJSON(&user) == nil {
+			user.Id = indexUser
+			users = append(users, user)
+			indexUser++
+			c.JSON(200, user)
+		} else {
+			c.JSON(400, gin.H{
+				"error": "Invalid payload",
+			})
+		}
+	})
+	//Eliminación de usuarios
+	router.DELETE("/api/users/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		idParsed, err := strconv.Atoi(id)
+		if err != nil {
+			c.JSON(400, gin.H{
+				"error": "Invalid id",
+			})
+			return
+		}
+		fmt.Println("Id a borrar: ", id)
+		for i, user := range users {
+			if user.Id == idParsed {
+				users = append(users[:i], users[i+1:]...)
+				c.JSON(200, gin.H{
+					"message": "User Deleted",
+				})
+				return
+			}
+		}
+		c.JSON(201, gin.H{})
+	})
+
+	router.Run(":8001")
 }
